8c2615c8ae1c87ee5d51f7e0b3e8dcb775f6657d
Bram Smulders <bram@supple.works>
2026-04-22 11:00:07 +0200

    chore: update sugarcube and use new fluid syntax

--- packages/web/sugarcube.config.ts
+++ packages/web/sugarcube.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "@sugarcube-sh/cli";
 
 export default defineConfig({
-	transforms: {
-		fluid: {
-			min: 360,
-			max: 1240,
+	variables: {
+		transforms: {
+			fluid: {
+				min: 360,
+				max: 1240,
+			},
 		},
 	},
 	utilities: {
-		color: {
-			source: "color.*",
-			prefix: "text",
+		classes: {
+			color: {
+				source: "color.*",
+				prefix: "text",
+			},
 		},
 	},
 });
